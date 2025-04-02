8f0e746511784c59ac9d36359604565f90b5fe63
Supakit Boonsongprasert <159343560+supakitboon@users.noreply.github.com>
2025-04-01 23:31:12 -0400

    Add files via upload

--- lab7/lab7b/main.tf
+++ lab7/lab7b/main.tf
@@ -1,1 +1,25 @@
 
+provider "aws" {
+  region = "us-east-2"
+}
+
+variable "instances" {
+  type = map
+  default = {
+    "web1" = "t2.micro"
+    "web2" = "t3.micro"
+    "web3" = "t2.small"
+  }
+}
+
+resource "aws_instance" "web" {
+  for_each = var.instances
+
+  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 for us-east-2
+  instance_type = each.value
+
+  tags = {
+    Name = each.key
+  }
+}
+
