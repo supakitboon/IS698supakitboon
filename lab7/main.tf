af4968bbec35fc0e32afc33f2030e84d97958a30
Supakit Boonsongprasert <159343560+supakitboon@users.noreply.github.com>
2025-04-01 22:49:45 -0400

    Upload Main.tf

--- lab7/main.tf
+++ lab7/main.tf
@@ -1,1 +1,23 @@
+terraform {
+  backend "s3" {
+    bucket         = "lab7-terraformstate"
+    key            = "terraform/state.tfstate"
+    region         = "us-east-2"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
+
+provider "aws" {
+  region = "us-east-2"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Terraform-Test-Instance"
+  }
+}
 
